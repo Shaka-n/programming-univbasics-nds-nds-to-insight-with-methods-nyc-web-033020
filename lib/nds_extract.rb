bce2953fad8b8f1d4414d02fe8a7275d2b814a4f
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-23 15:40:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,7 +22,8 @@ def directors_totals(nds)
   result = {}
   director_index = 0 
   while director_index < nds.length do
-    result[director_index][]
-  
+    result[director_index][:name] = gross_for_director(nds[director_index])
+    director_index += 1 
+  end
   result
 end
